f39f52bf178a0a6dda08d2d551d95a32c916ddaa
Priya!! <priyahariyale6@gmail.com>
2026-09-16 17:14:06 +0530

    Prepare server for Vercel deployment

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
-const config = require("./config");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 
 app.use("/api", routes);
@@ -15,6 +14,4 @@ app.use((error, req, res, next) => {
   res.status(500).json({ error: "Internal server error" });
 });
 
-app.listen(config.port, () => {
-  console.log(`LeadFlow AI API running on http://localhost:${config.port}`);
-});
+module.exports = app;
